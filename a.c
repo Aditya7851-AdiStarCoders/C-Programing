c7b296404ada8bf63abd8ad757a1a3cd29269d12
Aditya7851-AdiStarCoders <72146559+Aditya7851-AdiStarCoders@users.noreply.github.com>
2021-10-07 12:20:21 +0530

    Add  files c programs

---
+++ a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+  void main ()
+{
+    char a[50];
+    
+    printf("sadsadad");
+    scanf(" %c",&a);
+    printf("%c",a);
+  
+}
+
---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+
+int main ()
+{   char a[50];
+    std::cout<<"Aditya More ";
+    std::cin>>a;
+ system("cls");
+    std::cout<<"hi your name is :"<<a;
+
+
+
+    return 0;
+}
